80150a254e2648b2e74306f1698a2b4708f04e99
kyle kenney <kairukenney@gmail.com>
2016-08-25 11:45:01 -0700

    added skeleton for http call

--- app.js
+++ app.js
@@ -7,4 +7,14 @@ app.config(function($routeProvider){
 });
 app.controller('mainController', function($log){
     $log.info("I am ready to load!");
+    $http({
+        method: 'GET',
+        url: '/someUrl'
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
 });
