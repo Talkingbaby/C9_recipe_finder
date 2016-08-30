9d7a4c82e51ae5db62d6e176f42f9ca39a0a78cd
kyle kenney <kairukenney@gmail.com>
2016-08-30 08:35:11 -0700

    working http and played with input fields

--- app.js
+++ app.js
@@ -6,17 +6,26 @@ app.config(function($routeProvider){
         })
 });
 app.controller('mainController', function($http, $log){
-    $log.info("I am ready to load!");
-    $http({
-        method: 'GET',
-        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine=french&number=25&license=true',
-        headers: {
-            'X-mashape-key': 'VpQmAeJYO5msh7bVwZT13pUsanqKp1DU33NjsnvQ9KO5VtnlU9'
-        }
-    }).then(function successCallback(response) {
-        var results = response.data.results;
-        $log.info("Response: ", results);
-    }, function errorCallback(response) {
-        $log.error("Response: ", response);
-    });
+    //$log.info("I am ready to load!");
+    this.cuisine_array = ['french', 'vegan'];
+    this.cuisine = $('.cuisine').val();
+    this.cuisine_key = '?cuisine=' + this.cuisine;
+
+    $log.info('cuisine: ', this.cuisine);
+
+    this.get_recipe_info = function(){
+        $http({
+            method: 'GET',
+            url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search' + this.cuisine_key + '&number=25&license=true',
+            headers: {
+                'X-mashape-key': 'VpQmAeJYO5msh7bVwZT13pUsanqKp1DU33NjsnvQ9KO5VtnlU9'
+            }
+        }).then(function successCallback(response) {
+            var results = response.data.results;
+            $log.info("Response: ", results);
+        }, function errorCallback(response) {
+            $log.error("Response: ", response);
+        });
+    };
+
 });
