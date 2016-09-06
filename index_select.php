1faae61b8a3dbdbe9071323d0696c1878636c05b
emalfano <emalfano1@gmail.com>
2016-09-06 12:11:59 -0700

    add a php script to select from db

--- index_select.php
+++ index_select.php
@@ -1,8 +1,10 @@
 <!-- index_select.php -->
 <?php
 require_once('mysql_connect.php');
+//$query = "UPDATE `recipe_master` set `ready_in_minutes` = 30 WHERE id = 1";
 $query = "SELECT * FROM `recipe_master`";
 $result = mysqli_query($conn, $query);
+
 if(!empty($result) && mysqli_num_rows($result)>0){
     print('results!');
     while($single_row = mysqli_fetch_assoc($result)){
@@ -12,3 +14,14 @@ if(!empty($result) && mysqli_num_rows($result)>0){
     }
 }
 ?>
+<!--print('affected rows '.mysqli_affected_rows($conn));-->
+<!--if(mysqli_affected_rows($conn) > 0) {-->
+<!--        print('<br>table updated!');-->
+<!---->
+<!-- }elseif (mysqli_affected_rows($conn) == -1){-->
+<!--        print('bad query : '. mysqli_error($conn));-->
+<!-- } else {-->
+<!--        print('<br>table not updated! try again');-->
+<!-- }-->
+<!-- ?>-->
+
