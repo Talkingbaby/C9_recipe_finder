54bb04081694e17b73db6ee64e873619ae528007
emalfano <emalfano1@gmail.com>
2016-09-08 13:41:55 -0700

    add calls to update db

--- index_select.php
+++ index_select.php
@@ -1,7 +1,7 @@
 <!-- index_select.php -->
 <?php
 require_once('mysql_connect.php');
-//$query = "UPDATE `recipe_master` set `ready_in_minutes` = 30 WHERE id = 1";
+ 
 $query = "SELECT * FROM `recipe_master`";
 $result = mysqli_query($conn, $query);
 
@@ -14,14 +14,3 @@ if(!empty($result) && mysqli_num_rows($result)>0){
     }
 }
 ?>
-<!--print('affected rows '.mysqli_affected_rows($conn));-->
-<!--if(mysqli_affected_rows($conn) > 0) {-->
-<!--        print('<br>table updated!');-->
-<!---->
-<!-- }elseif (mysqli_affected_rows($conn) == -1){-->
-<!--        print('bad query : '. mysqli_error($conn));-->
-<!-- } else {-->
-<!--        print('<br>table not updated! try again');-->
-<!-- }-->
-<!-- ?>-->
-
